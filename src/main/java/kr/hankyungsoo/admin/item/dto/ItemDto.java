3db202971dfb7124ad0e94a15eff00f431bbcd22
kyungsoo han <hanks6160@gmail.com>
2023-08-20 22:16:53 +0900

    품목 form.th.xml 파일 추가 및 ItemDto파일 변경

--- src/main/java/kr/hankyungsoo/admin/item/dto/ItemDto.java
+++ src/main/java/kr/hankyungsoo/admin/item/dto/ItemDto.java
@@ -1,5 +1,9 @@
 package kr.hankyungsoo.admin.item.dto;
 
+import kr.hankyungsoo.admin.business.dto.BusinessDto;
+import kr.hankyungsoo.admin.item.domain.Item;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Value;
 
@@ -10,10 +14,32 @@ import java.time.LocalDateTime;
  * DTO for {@link kr.hankyungsoo.admin.item.domain.Item}
  */
 @Data
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ItemDto  {
-    String itemId;
-    String itemName;
-    String unit;
-    LocalDateTime expDate;
-    String memo;
+    private String itemId;
+    private String itemName;
+    private String unit;
+    private LocalDateTime expDate;
+    private BusinessDto businessDto;
+    private String memo;
+
+    public static ItemDto of(String itemId,
+                             String itemName,
+                             String unit,
+                             LocalDateTime expDate,
+                             BusinessDto businessDto,
+                             String memo) {
+        return new ItemDto(itemId, itemName, unit, expDate, businessDto, memo);
+    }
+
+    public static ItemDto from(Item entity){
+        return new ItemDto(
+                entity.getItemId(),
+                entity.getItemName(),
+                entity.getUnit(),
+                entity.getExpDate(),
+                BusinessDto.from(entity.getBusiness()),
+                entity.getMemo());
+    }
+
 }
