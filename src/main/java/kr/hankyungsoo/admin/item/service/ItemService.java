e5a5fc50982bca65d5957668dc3a19a2849e0ccc
kyungsoo han <hanks6160@gmail.com>
2023-08-20 23:41:44 +0900

    ItemService 클래스 추가

---
+++ src/main/java/kr/hankyungsoo/admin/item/service/ItemService.java
@@ -0,0 +1,16 @@
+package kr.hankyungsoo.admin.item.service;
+
+import kr.hankyungsoo.admin.item.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+
+}
