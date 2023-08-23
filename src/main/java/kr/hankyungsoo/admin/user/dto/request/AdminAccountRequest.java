c7b99e8830090eabec6ccf886613ceaf808ba79a
kyungsoo han <hanks6160@gmail.com>
2023-08-24 06:59:10 +0900

    비밀번호, 비밀번호확인 일치 확인을 위한 속성 추가

--- src/main/java/kr/hankyungsoo/admin/user/dto/request/AdminAccountRequest.java
+++ src/main/java/kr/hankyungsoo/admin/user/dto/request/AdminAccountRequest.java
@@ -5,16 +5,19 @@ import kr.hankyungsoo.admin.user.dto.AdminAccountDto;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.Set;
 
 @Data
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
 public class AdminAccountRequest {
 
      private String userId;
      private String userPassword;
+     private String userPassword2;
      private Set<RoleType> roleTypes;
      private String email;
      private String nickname;
@@ -24,12 +27,12 @@ public class AdminAccountRequest {
      private LocalDateTime modifiedAt;
      private String modifiedBy;
 
-    public static AdminAccountRequest of(String userId, String userPassword, Set<RoleType> roleTypes, String email, String nickname, String memo) {
-        return AdminAccountRequest.of(userId, userPassword, roleTypes, email, nickname, memo, null, null, null, null);
+    public static AdminAccountRequest of(String userId, String userPassword,String userPassword2, Set<RoleType> roleTypes, String email, String nickname, String memo) {
+        return AdminAccountRequest.of(userId, userPassword, userPassword2, roleTypes, email, nickname, memo, null, null, null, null);
     }
 
-    public static AdminAccountRequest of(String userId, String userPassword, Set<RoleType> roleTypes, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
-        return new AdminAccountRequest(userId, userPassword, roleTypes, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
+    public static AdminAccountRequest of(String userId, String userPassword, String userPassword2, Set<RoleType> roleTypes, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new AdminAccountRequest(userId, userPassword, userPassword2, roleTypes, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
     }
 
     public AdminAccountDto toDto() {
