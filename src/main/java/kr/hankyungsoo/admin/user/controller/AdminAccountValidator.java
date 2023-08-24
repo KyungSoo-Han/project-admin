a8002b148ba9f7789a9f296c9d1852063da92b10
kyungsoo han <hanks6160@gmail.com>
2023-08-24 23:13:30 +0900

    회원가입 뷰페이지 및 오류 메세지 처리

--- src/main/java/kr/hankyungsoo/admin/user/controller/AdminAccountValidator.java
+++ src/main/java/kr/hankyungsoo/admin/user/controller/AdminAccountValidator.java
@@ -4,6 +4,7 @@ package kr.hankyungsoo.admin.user.controller;
 import kr.hankyungsoo.admin.user.dto.AdminAccountDto;
 import kr.hankyungsoo.admin.user.dto.request.AdminAccountRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
@@ -18,6 +19,24 @@ public class AdminAccountValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         AdminAccountRequest adminAccountRequest = (AdminAccountRequest) target;
+
+        if(!StringUtils.hasText(adminAccountRequest.getUserId())){
+            errors.rejectValue("userId","required",null,null);
+        }
+        if(!StringUtils.hasText(adminAccountRequest.getEmail())){
+            errors.rejectValue("email","required",null,null);
+        }
+        if(!StringUtils.hasText(adminAccountRequest.getNickname())){
+            errors.rejectValue("nickname","required",null,null);
+        }
+        if(!StringUtils.hasText(adminAccountRequest.getUserPassword())){
+            errors.rejectValue("userPassword","required",null,null);
+        }
+        if(!StringUtils.hasText(adminAccountRequest.getUserPassword2())){
+            errors.rejectValue("userPassword2","required",null,null);
+        }
+
+
         if(!adminAccountRequest.getUserPassword().equals(adminAccountRequest.getUserPassword2())){
             errors.rejectValue("userPassword","this-not-equal",new Object[]{"비밀번호"},"비밀번호가 맞지 않습니다.");
         }
