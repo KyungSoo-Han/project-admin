232adf09b8374b3d0f4f48b05ff25aaa52f6a381
kyungsoo han <hanks6160@gmail.com>
2023-09-01 00:12:19 +0900

    품목 삭제 테스트 코드 작성

---
+++ src/test/java/kr/hankyungsoo/admin/item/service/ItemServiceTest.java
@@ -0,0 +1,32 @@
+package kr.hankyungsoo.admin.item.service;
+
+import kr.hankyungsoo.admin.item.repository.ItemRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.BDDMockito.*;
+import static org.mockito.BDDMockito.willDoNothing;
+
+@DisplayName("비즈니스 로직 - 품목")
+@ExtendWith(MockitoExtension.class)
+class ItemServiceTest {
+
+    @InjectMocks private ItemService sut;
+    @Mock private ItemRepository itemRepository;
+
+    @Test
+    void deleteItem() {
+        String itemId = "001";
+        Long businessId = 1L;
+
+        willDoNothing().given(itemRepository).deleteItem(itemId,businessId);
+
+        sut.deleteItem(itemId,businessId);
+
+        then(itemRepository).should().deleteItem(itemId,businessId);
+    }
+}
