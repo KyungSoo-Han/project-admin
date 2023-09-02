0fd74dfd24bc8612fb83cfa595397a9d1593997b
kyungsoo han <hanks6160@gmail.com>
2023-09-02 22:49:39 +0900

    해더 레이아웃 로그아웃url 변경

--- src/main/java/kr/hankyungsoo/admin/user/controller/AdminAccountController.java
+++ src/main/java/kr/hankyungsoo/admin/user/controller/AdminAccountController.java
@@ -7,6 +7,9 @@ import kr.hankyungsoo.admin.user.dto.request.AdminAccountRequest;
 import kr.hankyungsoo.admin.user.service.AdminAccountService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -18,6 +21,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.util.Collections;
@@ -39,6 +44,15 @@ public class AdminAccountController {
     @PostMapping("/login")
     public String login(@ModelAttribute("adminAccount") AdminAccountRequest request){
 
+        return "redirect:/";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth!=null){
+            new SecurityContextLogoutHandler().logout(request,response,auth);
+        }
         return "redirect:/";
     }
 
