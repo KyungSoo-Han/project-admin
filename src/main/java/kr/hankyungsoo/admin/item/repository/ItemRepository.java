fca550c7301d78425afaf8256c7c4d4835ffe432
kyungsoo han <hanks6160@gmail.com>
2023-09-03 22:21:57 +0900

    품목 엔티티 조회

--- src/main/java/kr/hankyungsoo/admin/item/repository/ItemRepository.java
+++ src/main/java/kr/hankyungsoo/admin/item/repository/ItemRepository.java
@@ -1,6 +1,7 @@
 package kr.hankyungsoo.admin.item.repository;
 
 import kr.hankyungsoo.admin.item.domain.Item;
+import kr.hankyungsoo.admin.item.dto.ItemDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,4 +16,7 @@ public interface ItemRepository extends JpaRepository<Item, String> {
 
     @Query("select MAX(CAST(i.itemId AS int)) + 1 from Item i where i.business.id = :businessId")
     int nextItemId(@Param("businessId") Long businessId);
+
+    @Query("select i from Item i where i.business.id = :businessId and i.itemId = :itemId ")
+    Item getItem(@Param("businessId") Long businessId, @Param("itemId")String itemId );
 }
